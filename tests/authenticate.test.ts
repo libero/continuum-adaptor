a613fb865cdba507b6a5ece1d97c96af7194b5ae
Cory <c.donkin@elifesciences.org>
2020-01-06 12:45:31 +0000

    add assertions for returned token

--- tests/authenticate.test.ts
+++ tests/authenticate.test.ts
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 import axios from 'axios';
 import { Server } from 'http';
 import { readFileSync } from 'fs';
 import * as express from 'express';
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 const configPath = `${__dirname}/config/continuum-auth.json`;
 const config = JSON.parse(readFileSync(configPath, 'utf8'));
 
@@ -14,7 +15,6 @@ describe('Authenticate', (): void => {
             resp.status(200).send('Successful redirect');
         });
         app.get('/profiles/*', (_, resp) => {
-            console.log('We hit this correctly.');
             resp.status(200).json({
                 id: 'TEST_ID',
                 name: {
@@ -22,12 +22,16 @@ describe('Authenticate', (): void => {
                     index: 'TEST_INDEX_NAME',
                 },
                 orcid: '0000-0001-2345-6789',
-                emailAddresses: [],
+                emailAddresses: [
+                    {
+                        value: 'test@example.com'
+                    }
+                ],
                 affiliations: [],
             });
         });
         //TODO: remove magic port number
-        server = app.listen(3002, () => console.log('Listening port 3002'));
+        server = app.listen(3002);
 
         // Do we need a health check on this awaited?
     });
@@ -51,14 +55,54 @@ describe('Authenticate', (): void => {
         );
 
         // Remeber response is redirect. We need to catch this correctly for assertions
-        await axios
-            .get(`http://localhost:3001/authenticate/${mockJournalToken}`)
-            .then(res => {
-                console.log('RESPONSE: ', res);
-            })
-            .catch((error: Error) => {
-                console.log('ERROR: ', error.stack);
-            });
+        await axios.get(`http://localhost:3001/authenticate/${mockJournalToken}`).then(res => {
+            expect(res.status).toBe(200);
+            expect(res.data).toBe('Successful redirect')
+
+            const [redirectUrl, returnedToken] = res.request.res.responseUrl.split('#');
+            expect(redirectUrl).toBe(config.login_return_url);
+            const verifiedReturnToken = verify(returnedToken, config.authentication_jwt_secret);
+
+            const expectedPayload =     {
+                "token_version": "0.1-alpha",
+                "identity": {
+                    "external": [
+                        {
+                            "id": "TEST_ID",
+                            "domain": "elife-profiles"
+                        },
+                        {
+                            "id": "0000-0001-2345-6789",
+                            "domain": "orcid"
+                        }
+                    ]
+                },
+                "roles": [
+                    {
+                        "journal": "elife",
+                        "kind": "author"
+                    }
+                ],
+                "meta": {
+                    "email": "test@example.com"
+                },
+                "iss": "continuum-auth"
+            };
+
+            expect(typeof verifiedReturnToken['iat']).toBe('number');
+            delete verifiedReturnToken['iat'];
+
+            expect(typeof verifiedReturnToken['exp']).toBe('number');
+            delete verifiedReturnToken['exp'];
+
+            expect(typeof verifiedReturnToken['identity']['user_id']).toBe('string');
+            delete verifiedReturnToken['identity']['user_id'];
+
+            expect(typeof verifiedReturnToken['token_id']).toBe('string');
+            delete verifiedReturnToken['token_id'];
+
+            expect(verifiedReturnToken).toStrictEqual(expectedPayload);
+        });
         // unwrap token assert data from profile correct
     });
     // No token
