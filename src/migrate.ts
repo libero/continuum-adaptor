4795fa0e15819661689c1d1bfbb46fa6828f5d8e
Erez Odier <erez.odier@gmail.com>
2020-01-14 10:03:55 +0000

    Add migration for user and identity tables
    The @libero/migrator package has been added along with migrate script entry in the package.json
    The original xpub schema for the user and identity tables has been kept.
    
    re #34

---
+++ src/migrate.ts
@@ -0,0 +1,14 @@
+import { Cli } from '@libero/migrator';
+import config from './config';
+
+const cli = new Cli({
+    banner: 'Continuum Adaptor Service: Migration tool',
+    name: 'migrate',
+    knexConfig: config.knex,
+    migrations: {
+        path: `${__dirname}/migrations`,
+        pattern: /.*\.js$/,
+    },
+});
+
+cli.exec();
