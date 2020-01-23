68cb39cc2e2237db82772d4e7ebd6a7ee10adfa0
Noris Alsaadie <norisalsaadie@users.noreply.github.com>
2020-01-23 12:56:42 +0000

    Add test case from add_event_bus_integration_test

--- tests/authenticate.test.ts
+++ tests/authenticate.test.ts
@@ -1,5 +1,7 @@
-/* eslint-disable prettier/prettier */
 import axios from 'axios';
+import { RabbitEventBus } from '@libero/event-bus';
+import { LiberoEventType } from '@libero/event-types';
+import waitForExpect from 'wait-for-expect';
 import { login, LoginReturnValue, config } from './utils';
 
 describe('Authenticate', (): void => {
@@ -7,7 +9,7 @@ describe('Authenticate', (): void => {
     it('authenticates a user session token', async (): Promise<void> => {
         await login().then(({ res, redirectUrl, decodedToken }: LoginReturnValue) => {
             expect(res.status).toBe(200);
-            expect(res.data).toBe('Redirect reached successfully')
+            expect(res.data).toBe('Redirect reached successfully');
             expect(redirectUrl).toBe(config.login_return_url);
 
             const expectedPayload = {
@@ -31,16 +33,42 @@ describe('Authenticate', (): void => {
     });
 
     it('rejects request when no token passed', async (): Promise<void> => {
-        await axios.get('http://localhost:3001/authenticate').catch(({response}) => {
+        await axios.get('http://localhost:3001/authenticate').catch(({ response }) => {
             expect(response.status).toBe(401);
-            expect(response.data).toEqual({ ok: false, msg: 'No token' })
+            expect(response.data).toEqual({ ok: false, msg: 'No token' });
         });
     });
 
     it('rejects request when invalid token passed', async (): Promise<void> => {
-        await axios.get('http://localhost:3001/authenticate/INVALID_TOKEN').catch(({response}) => {
+        await axios.get('http://localhost:3001/authenticate/INVALID_TOKEN').catch(({ response }) => {
             expect(response.status).toBe(401);
-            expect(response.data).toEqual({ ok: false, msg: 'Invalid token' })
+            expect(response.data).toEqual({ ok: false, msg: 'Invalid token' });
         });
+    });
+
+    it('sends the apropriate message to the message bus when user is authenticated', async (done): Promise<void> => {
+        jest.setTimeout(25000); // to avoid jest timeout on CI env
+        const url = `amqp://localhost`;
+        const eventBus = new RabbitEventBus({ url }, [LiberoEventType.userLoggedInIdentifier], 'continuum-auth');
+        await eventBus.connect();
+        let payload;
+        await eventBus.subscribe(
+            LiberoEventType.userLoggedInIdentifier,
+            (event): Promise<boolean> => {
+                payload = event.payload['result'];
+                return Promise.resolve(true);
+            },
+        );
+        await login();
+
+        await waitForExpect(
+            async () => {
+                expect(payload).toBe('authorized');
+                await eventBus.destroy();
+                done();
+            },
+            23000,
+            1000,
+        );
     });
 });
