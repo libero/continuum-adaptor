c32cf929c4db375d5f4eef34adba7c4978b3f7d6
erezmus <e.odier@elifesciences.org>
2020-05-27 17:04:24 +0100

    add person endpoint

---
+++ src/use-cases/getPerson.test.ts
@@ -0,0 +1,80 @@
+import { Option } from 'funfix';
+import * as jwt from '../jwt';
+import { Request, Response, NextFunction } from 'express';
+import { Config as KnexConfig } from 'knex';
+import * as flushPromises from 'flush-promises';
+import { Config } from '../config';
+import { PeopleRepository, Person } from '../repo/people';
+import { GetPerson } from './getPerson';
+
+jest.mock('../logger');
+
+const config: Config = {
+    port: 3000,
+    login_url: 'login-url',
+    login_return_url: 'login-return',
+    authentication_jwt_secret: 'ca-secret',
+    continuum_jwt_secret: 'continuum-secret',
+    continuum_api_url: 'somewhere',
+    knex: {} as KnexConfig,
+};
+
+const person = {
+    id: 'profile_id',
+    name: {
+        preferred: 'Joe Bloggs',
+        index: 'Bloggs, Joe',
+    },
+    type: {
+        id: 'reviewing-editor',
+        label: 'Reviewing Editor',
+    },
+} as Person;
+
+describe('Get Editors', (): void => {
+    let requestMock;
+    let responseMock;
+    let nextFunctionMock;
+    let peopleServiceMock;
+    let handler;
+
+    beforeEach((): void => {
+        requestMock = {
+            header: jest.fn(),
+        };
+        responseMock = {
+            status: jest.fn(),
+            json: jest.fn(),
+        };
+        peopleServiceMock = {
+            getPersonById: jest.fn(),
+        };
+        nextFunctionMock = jest.fn();
+        peopleServiceMock.getPersonById.mockImplementation(() => Promise.resolve(Option.of(person)));
+
+        responseMock.status.mockImplementation(() => responseMock);
+
+        handler = GetPerson(config, peopleServiceMock as PeopleRepository);
+    });
+
+    it('should call people service correctly', async () => {
+        const decodeTokenMock = jest.spyOn(jwt, 'decodeToken');
+        decodeTokenMock.mockImplementation(() => Option.of(({ sub: 'id' } as unknown) as jwt.LiberoAuthToken));
+        requestMock.header.mockImplementation(() => 'Bearer: Valid Token');
+        requestMock.params = { id: 'profile_id' };
+
+        handler(
+            requestMock as Request,
+            (responseMock as unknown) as Response,
+            (nextFunctionMock as unknown) as NextFunction,
+        );
+
+        await flushPromises();
+
+        expect(responseMock.status).toHaveBeenCalledWith(200);
+        expect(responseMock.json).toHaveBeenCalled();
+        expect(responseMock.json.mock.calls[0][0]).toMatchObject(person);
+        expect(peopleServiceMock.getPersonById).toHaveBeenCalledTimes(1);
+        expect(peopleServiceMock.getPersonById).toHaveBeenCalledWith('profile_id');
+    });
+});
---
+++ src/use-cases/getPerson.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from 'express';
+import { Unauthorized } from 'http-errors';
+import { Config } from '../config';
+import { PeopleRepository } from '../repo/people';
+import { DomainLogger } from '../logger';
+import { decodeToken, LiberoAuthToken } from '../jwt';
+
+export const GetPerson = (config: Config, peopleRepo: PeopleRepository) => async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<void | Response> => {
+    try {
+        const authHeader = req.header('Authorization');
+
+        if (!authHeader) {
+            throw new Unauthorized('Invalid token');
+        }
+        const token = authHeader.split(' ')[1];
+
+        if (!token) {
+            throw new Unauthorized('Invalid token');
+        }
+
+        const maybeDecodedToken = decodeToken(config.authentication_jwt_secret, token);
+
+        if (maybeDecodedToken.isEmpty()) {
+            throw new Unauthorized('Invalid token');
+        }
+
+        const userId = (maybeDecodedToken.get() as LiberoAuthToken).sub;
+
+        if (!userId) {
+            throw new Unauthorized('Invalid token');
+        }
+
+        const payload = await peopleRepo.getPersonById(req.params.id);
+
+        return res.status(200).json(payload.get());
+    } catch (error) {
+        DomainLogger.info(error.stack);
+        return next(error);
+    }
+};
